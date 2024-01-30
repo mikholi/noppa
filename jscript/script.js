2ea5f879704920cf18e87cd70b53f1539e41cd1f
mikholi <keranemi@icloud.com>
2024-01-30 18:32:28 +0200

    .

---
+++ jscript/script.js
@@ -0,0 +1,14 @@
+document.querySelector('#dice').addEventListener('click', () => {
+    const noppakuva = document.getElementById('noppakuva');
+    const randomizedNumber = getRandomNumberInRange(1, 6);
+    
+    
+    const imagePath = `./img/${randomizedNumber}.png`;
+
+    
+    noppakuva.src = imagePath;
+});
+
+const getRandomNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
